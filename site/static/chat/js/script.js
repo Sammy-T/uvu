27ea19a49b33c9f79def21da55573fc0737286ef
Sammy-T <cyam9019@aim.com>
2021-02-11 23:14:09 -0500

    Create peer connections

--- site/static/chat/js/script.js
+++ site/static/chat/js/script.js
@@ -1,12 +1,22 @@
+const configuration = {
+    iceServers: [
+        {urls: 'stun:stun.l.google.com:19302'}
+    ]
+};
+
 const db = firebase.firestore();
 
 let roomRef = null;
 let connectionsRef = null;
 
-let createdRoom = false;
-
 let localUid = Math.random().toString(36).slice(-8);
 let username = null;
+
+const offerTimes = {};
+const answerTimes = {};
+
+const peerConnections = {};
+const dataChannels = {};
 
 let modalAction = null;
 
@@ -35,9 +45,7 @@ async function createRoom() {
     const res = await roomRef.set(roomData);
     console.log(`Created room. id: ${roomRef.id}`, res);
 
-    createdRoom = true;
-
-    //// TODO: Add negotiator
+    addNegotiator();
 
     // Update the UI
     createRoomBtn.disabled = true;
@@ -81,7 +89,10 @@ async function joinRoom() {
     const res = await roomRef.update(updateData);
     console.log('Updated room.', res);
 
-    //// TODO: Add negotiator / create offers
+    // Create offers
+    roomData.participants.forEach(participant => createOffer(participant));
+
+    addNegotiator();
 
     // Update the UI
     createRoomBtn.disabled = true;
@@ -90,8 +101,221 @@ async function joinRoom() {
     videoOptions.forEach(optionEl => optionEl.disabled = true);
 }
 
+function addNegotiator() {
+    //// TODO: Store listener's unsub and call when unneeded
+    connectionsRef.onSnapshot(snapshot => {
+        snapshot.forEach(async doc => {
+            const data = doc.data();
+            console.log(doc.id, '=>', doc.data());
+
+            if(data.from && data.from === localUid && data.answer && (!answerTimes[data.to] || answerTimes[data.to].getTime() != data.answerTime.toDate().getTime())) {
+                console.log('Received new answer.', data.answer, data);
+
+                const participant = data.to;
+                const peerConnection = peerConnections[participant];
+
+                // Update the participant's local answer time
+                answerTimes[participant] = data.answerTime.toDate();
+
+                const remoteDesc = new RTCSessionDescription(data.answer);
+                await peerConnection.setRemoteDescription(remoteDesc);
+            }else if(data.to && data.to === localUid && data.offer && (!offerTimes[data.from] || offerTimes[data.from].getTime() != data.offerTime.toDate().getTime())) {
+                console.log('Received new offer.', data.offer, data);
+
+                const participant = data.from;
+
+                // Update the participant's local offer time
+                offerTimes[participant] = data.offerTime.toDate();
+
+                createAnswer(participant, doc);
+            }
+        });
+    }, error => {
+        console.error('Error listening to connections collection.\n', error);
+    });
+}
+
+async function createOffer(participant) {
+    // Create a connection
+    const peerConnection = new RTCPeerConnection(configuration);
+    peerConnections[participant] = peerConnection; // Map the connection to the participant
+
+    registerPeerConnectionListeners(participant, peerConnection);
+
+    // Create a data channel on the connection
+    // (A channel or stream must be present for ICE candidate events to fire)
+    const dataChannel = peerConnection.createDataChannel('messages');
+    dataChannel[participant] = dataChannel; // Map the data channel to the participant
+
+    registerDataChannelListeners(participant, dataChannel);
+
+    const connectionDocRef = connectionsRef.doc(); // Create connection doc ref
+
+    // Start collecting ICE candidates
+    await collectIceCandidates(connectionDocRef, participant, peerConnection);
+
+    // Create an offer and use it to set the local description
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    console.log(participant, 'Created offer', offer);
+
+    offerTimes[participant] = new Date(); // Map the offer time to the participant
+
+    const connectionData = {
+        from: localUid,
+        to: participant,
+        offerTime: firebase.firestore.Timestamp.fromDate(offerTimes[participant]),
+        offer: {
+            type: offer.type,
+            sdp: offer.sdp
+        },
+        answerTime: null,
+        answer: null
+    };
+
+    // Send the offer to the signaling channel
+    const res = await connectionDocRef.set(connectionData);
+    console.log(participant, `Created connection doc with offer. id: ${connectionDocRef.id}`, res);
+}
+
+async function createAnswer(participant, connectionDoc) {
+    // Create a connection
+    const peerConnection = new RTCPeerConnection(configuration);
+    peerConnection[participant] = peerConnection; // Map the connection to the participant
+
+    registerPeerConnectionListeners(participant, peerConnection);
+
+    // Listen for data channels
+    peerConnection.addEventListener('datachannel', event => {
+        const dataChannel = event.channel;
+        dataChannels[participant] = dataChannel; // Map the data channel to the participant
+
+        registerDataChannelListeners(participant, dataChannel);
+    });
+
+    // Start collecting ICE candidates
+    await collectIceCandidates(connectionDoc.ref, participant, peerConnection);
+
+    const connectionData = connectionDoc.data();
+
+    // Use the offer to create the remote description
+    const remoteDesc = new RTCSessionDescription(connectionData.offer);
+    await peerConnection.setRemoteDescription(remoteDesc);
+
+    // Create an answer and use it to set the local description
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    answerTimes[participant] = new Date();
+
+    const answerData = {
+        answerTime: firebase.firestore.Timestamp.fromDate(answerTimes[participant]),
+        answer: {
+            type: answer.type,
+            sdp: answer.sdp
+        }
+    };
+
+    // Send the answer to the signaling channel
+    const res = await connectionDoc.ref.update(answerData);
+    console.log(participant, `Updated connection doc with answer. id: ${connectionDoc.ref.id}`, res);
+}
+
+async function collectIceCandidates(connectionDocRef, participant, peerConnection) {
+    const localCandidatesColl = connectionDocRef.collection(localUid);
+    const remoteCandidatesColl = connectionDocRef.collection(participant);
+
+    peerConnection.addEventListener('icecandidate', event => {
+        if(event.candidate) {
+            console.log(participant, 'Got candidate', event.candidate);
+
+            // Send candidate to signaling channel
+            localCandidatesColl.add(event.candidate.toJSON());
+        }
+    });
+
+    //// TODO: Map listener's unsub to object and call when unneeded?
+    // Listen to signaling channel for remote candidates
+    remoteCandidatesColl.onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(async change => {
+            if(change.type === 'added') {
+                const data = change.doc.data();
+                console.log(participant, 'Got remote candidate', data);
+
+                try {
+                    await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+                } catch (error) {
+                    console.error(participant, 'Error adding remote ICE candidate', error);
+                }
+            }
+        });
+    });
+}
+
+function registerPeerConnectionListeners(participant, peerConnection) {
+    peerConnection.addEventListener('negotiationneeded', async event => {
+        console.log(participant, 'Negotiation needed');
+
+        //// TODO: Create new offers to connected participants
+    });
+
+    peerConnection.addEventListener('icegatheringstatechange', event => {
+        console.log(participant, `ICE gathering state change: ${peerConnection.iceGatheringState}`);
+    });
+
+    peerConnection.addEventListener('connectionstatechange', event => {
+        console.log(participant, `Connection state change: ${peerConnection.connectionState}`);
+    });
+
+    peerConnection.addEventListener('signalingstatechange', event => {
+        console.log(participant, `Signaling state change: ${peerConnection.signalingState}`);
+    });
+
+    peerConnection.addEventListener('iceconnectionstatechange', event => {
+        console.log(participant, `ICE connection state change: ${peerConnection.iceConnectionState}`);
+    });
+}
+
+function registerDataChannelListeners(participant, dataChannel) {
+    dataChannel.addEventListener('open', event => {
+        console.log(participant, 'Data channel open');
+    });
+
+    dataChannel.addEventListener('close', event => {
+        console.log(participant, 'Data channel close');
+    });
+
+    dataChannel.addEventListener('message', event => {
+        console.log(participant, 'Message received: ', event.data);
+    });
+}
+
 function hangUp() {
+    //// TODO: Cleanup unsubs
+
     cleanUpDb();
+
+    for(const participant in offerTimes) {
+        delete offerTimes[participant];
+    }
+
+    for(const participant in answerTimes) {
+        delete answerTimes[participant];
+    }
+
+    for(const participant in dataChannels) {
+        const dataChannel = dataChannels[participant];
+        dataChannel.close();
+
+        delete dataChannels[participant]; // Remove the data channel from the global variable
+    }
+
+    for(const participant in peerConnections) {
+        const peerConnection = peerConnections[participant];
+        peerConnection.close();
+
+        delete peerConnections[participant]; // Remove the peer connection from the global variable
+    }
 
     // Update the UI
     createRoomBtn.disabled = false;
@@ -101,6 +325,26 @@ function hangUp() {
 }
 
 async function cleanUpDb() {
+    // Retrieve connections authored by this uid
+    const authoredConnections = await connectionsRef.where('from', '==', localUid).get();
+
+    authoredConnections.forEach(async connection => {
+        const remoteUid = connection.data().to; // Retrieve the participant's uid
+
+        const localCandidates = await connection.ref.collection(localUid).get();
+        const remoteCandidates = await connection.ref.collection(remoteUid).get();
+
+        const batch = db.batch();
+
+        // Delete each connection's candidate docs
+        localCandidates.forEach(candidate => batch.delete(candidate.ref));
+        remoteCandidates.forEach(candidate => batch.delete(candidate.ref));
+
+        batch.delete(connection.ref); // Delete the connection doc
+
+        await batch.commit();
+    });
+
     const roomDoc = await roomRef.get();
 
     if(roomDoc.exists) {
